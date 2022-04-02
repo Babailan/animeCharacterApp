c9696cff9c9558f1842954fa00c49cd7bf0bc3df
babailan <ronnelbabailan@outlook.com>
2022-04-02 17:03:39 +0800

    courasel

--- src/components/anime/animeRec.tsx
+++ src/components/anime/animeRec.tsx
@@ -2,35 +2,22 @@
 
 import Cards from "../cardRecommendations";
 import styles from "../../style/Recommendations.module.css";
-import { useRouter } from "next/router";
 
 function AnimeRecomendation({ animeRec }) {
-  const router = useRouter();
-  const onClickBox = (e: any, mal_id: string) => {
-    e.preventDefault();
-    router.push(`/anime/id/${mal_id}`, undefined, { shallow: true });
-  };
   return (
     <div className={styles.Recommendation_container}>
       <h1 className={styles.titles}>Anime Recommendations</h1>
       <div className={styles.Recommendation_list}>
-        {animeRec.map(({ mal_id, entry }) => {
+        {animeRec.map(({ mal_id, entry }, index: number) => {
           return (
-            <div
-              style={{
-                height: "fit-content",
-                width: "fit-content",
-              }}
-              onClick={(e) => onClickBox(e, entry[0].mal_id)}
+            <Cards
               key={mal_id}
-            >
-              <Cards
-                key={mal_id}
-                mal_id={mal_id}
-                images={entry[0].images.webp.image_url}
-                name={entry[0].title}
-              />
-            </div>
+              mal_id={mal_id}
+              images={entry[0].images.webp.image_url}
+              name={entry[0].title}
+              index={index}
+              lastId={animeRec.length - 1}
+            />
           );
         })}
       </div>
--- src/components/cardRecommendations.tsx
+++ src/components/cardRecommendations.tsx
@@ -1,49 +1,88 @@
-import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import { Card, CardActionArea, Tooltip } from "@mui/material";
+import { useState } from "react";
+import styles from "../style/Recommendations.module.css";
 import Image from "next/image";
+import { FaAngleRight } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 type cardProps = {
   images?: any;
   name?: string;
-  mal_id?: number;
+  mal_id?: any;
   id?: any;
+  index?: number;
+  lastId?: number;
 };
 
-function Cards({ images, name, mal_id, id }: cardProps) {
+function Cards({ images, name, mal_id, id, index, lastId }: cardProps) {
+  const router = useRouter();
+
+  let [scrollAmount, setScrollAmount] = useState(0);
+  const onClickBox = (e: any, mal_id: string) => {
+    e.preventDefault();
+    router.push(`/anime/id/${mal_id}`, undefined, { shallow: true });
+  };
+
+  function scrollToRight(slider: any) {
+    if (scrollAmount <= slider.scrollWidth - slider.clientWidth) {
+      slider.scrollTo({
+        top: 0,
+        left: (scrollAmount += slider.clientWidth),
+        behavior: "smooth",
+      });
+      setScrollAmount((p) => p + slider.clientWidth);
+    }
+  }
+
   return (
     <>
-      <Card
-        sx={{
-          maxWidth: 225,
-          minWidth: 225,
-          justifyContent: "flex-start",
-          alignContent: "flex-start",
-          bgcolor: "transparent",
+      <div
+        style={{
+          height: "fit-content",
+          width: "fit-content",
         }}
-        key={mal_id || id}
+        onClick={(e) => onClickBox(e, mal_id)}
+        key={mal_id}
       >
-        <CardActionArea
-          sx={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignContent: "flex-start",
-          }}
+        <Tooltip title={name} arrow placement={"top"}>
+          <Card
+            sx={{
+              maxWidth: 150,
+              minWidth: 150,
+              justifyContent: "flex-start",
+              alignContent: "flex-start",
+              bgcolor: "transparent",
+            }}
+            key={mal_id || id}
+          >
+            <CardActionArea
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                alignContent: "flex-start",
+              }}
+            >
+              <Image
+                src={images}
+                loading={"lazy"}
+                height="300px"
+                width={"225px"}
+                alt={id}
+              />
+            </CardActionArea>
+          </Card>
+        </Tooltip>
+      </div>
+      {index === lastId ? (
+        <div
+          className={styles.AngleRight}
+          onClick={(e) => scrollToRight(e.currentTarget.parentNode)}
         >
-          <Image
-            src={images}
-            loading={"lazy"}
-            height="300px"
-            width={"225px"}
-            alt={id}
-          />
-          <CardContent>
-            <Typography color={"#cdcdcd"} component={"h6"}>
-              {name}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+          <FaAngleRight />
+        </div>
+      ) : null}
     </>
   );
 }
